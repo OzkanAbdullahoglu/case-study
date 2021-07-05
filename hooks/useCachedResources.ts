20fb296c6b23552312131926ad2099295ed2a663
Ozkan Abdullahoglu <ozkanabdullahoglu@gmail.com>
2021-07-05 03:39:04 +0300

    feat: implemented hook updates

--- hooks/useCachedResources.ts
+++ hooks/useCachedResources.ts
@@ -3,22 +3,21 @@ import * as Font from 'expo-font';
 import * as SplashScreen from 'expo-splash-screen';
 import * as React from 'react';
 
-export default function useCachedResources() {
+export default function useCachedResources(): boolean {
   const [isLoadingComplete, setLoadingComplete] = React.useState(false);
 
-  // Load any resources or data that we need prior to rendering the app
   React.useEffect(() => {
     async function loadResourcesAndDataAsync() {
       try {
         SplashScreen.preventAutoHideAsync();
-
         // Load fonts
         await Font.loadAsync({
           ...Ionicons.font,
-          'space-mono': require('../assets/fonts/SpaceMono-Regular.ttf'),
+          'gotham-book': require('../assets/fonts/Gotham-Book.otf'),
+          'gotham-medium': require('../assets/fonts/Gotham-Medium.otf'),
+          'gotham-light': require('../assets/fonts/Gotham-Light.otf'),
         });
       } catch (e) {
-        // We might want to provide this error information to an error reporting service
         console.warn(e);
       } finally {
         setLoadingComplete(true);
