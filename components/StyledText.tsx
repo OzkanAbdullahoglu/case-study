83340c11838070127356b2dc00c466cadf38f8b6
Ozkan Abdullahoglu <ozkanabdullahoglu@gmail.com>
2021-07-05 03:39:50 +0300

    feat: styled text with font family

--- components/StyledText.tsx
+++ components/StyledText.tsx
@@ -1,7 +1,37 @@
+/* eslint-disable react-native/no-inline-styles */
 import * as React from 'react';
 
-import { Text, TextProps } from './Themed';
+import { TextPrimary, TextSecondary, TextProps } from './Themed';
 
-export function MonoText(props: TextProps) {
-  return <Text {...props} style={[props.style, { fontFamily: 'space-mono' }]} />;
+export function MonoText(props: TextProps): JSX.Element {
+  return (
+    <TextPrimary
+      {...props}
+      style={[props.style, { fontFamily: 'gotham-book' }]}
+    />
+  );
+}
+export function MediumText(props: TextProps): JSX.Element {
+  return (
+    <TextPrimary
+      {...props}
+      style={[props.style, { fontFamily: 'gotham-medium' }]}
+    />
+  );
+}
+export function MediumTextSecondary(props: TextProps): JSX.Element {
+  return (
+    <TextSecondary
+      {...props}
+      style={[props.style, { fontFamily: 'gotham-medium' }]}
+    />
+  );
+}
+export function LightText(props: TextProps): JSX.Element {
+  return (
+    <TextSecondary
+      {...props}
+      style={[props.style, { fontFamily: 'gotham-light', fontWeight: '800' }]}
+    />
+  );
 }
